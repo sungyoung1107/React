0cc6af4ba9fd485e4d1d664f455595ccd63c9b19
sungyoung1107 <dong09161107@gmail.com>
2023-04-13 18:46:13 +0900

    2023.04.13

---
+++ 6번째 실습과제/ScoreListBack.js
@@ -0,0 +1,18 @@
+// 서버단
+let express = require("express"); // node_modules 폴더에 있으면 경로를 지정하지 않아도 된다.
+let router = express.Router();
+let commonDB = require("./commonDB"); // ./ DB정보는 스키마 score로 가져온다.
+router.use(express.urlencoded({ extended: false }));
+
+/* http://127.0.0.1:3000/score */
+/* GET home page. */
+
+/* 기본 페이지 */
+router.get("/list", async function (req, res, next) {
+  let sql = `select id, user_name, kor, eng, mat from tb_score`;
+  let results = await commonDB.mysqlRead(sql, []);
+  console.log(results);
+  res.json({ scoreList: results });
+});
+
+module.exports = router;
